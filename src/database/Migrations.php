d5d562d70415bebaaf0a77aafc07b47a49b80557
hptsilva <humbertot@hotmail.com.br>
2025-04-29 11:04:11 -0300

    Corrigindo texto de mensagem de erro ao realizar migrações.

--- src/database/Migrations.php
+++ src/database/Migrations.php
@@ -25,13 +25,13 @@ class Migrations
         user_id int NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
-    )";
+        )";
 
         $query_created_table_blacklist = "CREATE TABLE blacklist_tokens (
         token VARCHAR(300) PRIMARY KEY NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )";
+        )";
 
         $connection = new ConnectionDB();
         $cnx = $connection->connect($_ENV['DB_HOSTNAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
@@ -48,10 +48,8 @@ class Migrations
             return "\033[32mMigrations performed successfully.\033[0m\n";
         } catch (PDOException | Exception $e) {
             // Registre o erro e relance a exceção
-            error_log("\033[31m".$e->getMessage()."\033[0m\n");
+            error_log("\033[31m".$e->getMessage()."\033[0m");
             die;
         }
-
     }
-
 }
